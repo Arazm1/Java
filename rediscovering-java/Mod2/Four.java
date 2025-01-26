bf94b94c79fe5d6ec2837ca052d530aefd2a2891
Arazm1 <arazm@metropolia.fi>
2025-01-26 22:06:05 +0200

    Add files via upload

---
+++ rediscovering-java/Mod2/Four.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Four {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter first number: ");
+        double first = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double second = scanner.nextDouble();
+
+        System.out.print("Enter third number: ");
+        double third = scanner.nextDouble();
+
+        double sum = first + second + third;
+        double product = first * second * third;
+        double average = sum / 3;
+
+        System.out.println("Sum is " + sum + ".");
+        System.out.println("product is " + product + ".");
+        System.out.println("Average is " + average + ".");
+        scanner.close();
+    }
+}
