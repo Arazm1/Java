ed67d6092e06e0408addc745cc5ea14e7abe868a
Arazm1 <arazm@metropolia.fi>
2025-01-27 09:02:45 +0200

    Update ThreeOne.java

--- rediscovering-java/Mod3/ThreeOne.java
+++ rediscovering-java/Mod3/ThreeOne.java
@@ -1,3 +1,8 @@
+//Kirjoita ohjelma, joka kysyy kalastajalta kuhan pituuden senttimetreinä. 
+//Jos kuha on alamittainen, ohjelma käskee laskea kuhan takaisin järveen ilmoittaen samalla käyttäjälle, 
+//montako senttiä alimmasta sallitusta pyyntimitasta puuttuu. Kuha on alamittainen, jos sen pituus on alle 37 cm.
+
+
 import java.util.Scanner;
 
 public class ThreeOne {
