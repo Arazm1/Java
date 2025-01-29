b6b3e6a922ea686b1af9c93493412d9a4d196bfd
Arazm1 <arazm@metropolia.fi>
2025-01-29 23:37:17 +0200

    Add files via upload

---
+++ Codecademy/Input and Output Lesson/Introduction2.java
@@ -0,0 +1,14 @@
+//The Scanner Class
+
+import java.util.Scanner;
+
+public class Introduction2 {
+  public static void main( String[] args ) {
+
+    Scanner input = new Scanner(System.in);
+
+    
+  }
+}
+
+  
