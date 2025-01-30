2d6c7de3ea820f4e97a614280a86dad7dcd63c89
Arazm1 <arazm@metropolia.fi>
2025-01-30 20:25:10 +0200

    Add files via upload

---
+++ Java - Small Programs/LogIn/Main.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+
+    static void loginUsername() { 
+        String username = "";
+
+        while (username.equals("")) {
+            try {
+                System.out.print("Enter a username: ");
+                username = scanner.nextLine();
+
+                if (username.equals("")) {
+                    System.out.println("You must enter a username!");
+                } else {
+                    System.out.println("Username selected: " + username);
+                    loginPassword();
+                }
+            } catch (Exception e) {
+                System.out.println("Error. Please try again");
+
+            }
+
+        }
+        scanner.close();
+    }
+
+    static String loginPassword() {
+        
+        String password;
+
+        while (true) {
+            System.out.print("Enter a password: ");
+            password = scanner.nextLine();
+
+            if (password.isEmpty() || password.length() <= 3) {
+                System.out.println("Password cannot be empty or less than 4 characters!");
+            } else {
+                System.out.println("Password selected: " + password);
+                return password;
+
+            }
+            
+        }
+
+        
+    }
+
+    public static void main(String[] args) {
+        loginUsername();
+
+        scanner.close(); // Close scanner at the end
+    }
+
+}
